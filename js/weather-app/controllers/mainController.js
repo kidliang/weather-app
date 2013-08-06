3c45556c88a5f8b3dec04d5ffff32980e6b8cd67
Simon Bingham <simon@Simon-Binghams-iMac.local>
2013-08-06 14:02:23 +0100

    no message

--- js/weather-app/controllers/mainController.js
+++ js/weather-app/controllers/mainController.js
@@ -1,10 +1,10 @@
 app.controller("mainController", function($scope, $http){
-	$scope.location = 'exeter';
-
-	$scope.init = function() {
+	$scope.data = null;
+	$scope.location = null;
+	$scope.submit = function() {
 		$http.jsonp('http://api.openweathermap.org/data/2.5/weather?q=' + $scope.location + '&callback=JSON_CALLBACK')
 			.success(function(data) {
-				console.log(data);
+				$scope.data = data;
 			});
-	};
+	}
 });
