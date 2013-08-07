87cfccdebfa508b7f90ba43a4622f7fba0a1d5dc
Simon Bingham <simon.bingham@ab-uk.com>
2013-08-07 14:05:23 +0100

    no message

--- js/weather-app/app.js
+++ js/weather-app/app.js
@@ -1,1 +1,20 @@
-var app = angular.module('WeatherApp',[]);
+var app = angular.module('WeatherApp',[])
+	.filter('customTimeFilter', function () {
+        return function (epochTime) {
+			var d = new Date(0);
+			d.setUTCSeconds(epochTime);
+			var hours = prependZeros(d.getHours(), 2);
+			var minutes = prependZeros(d.getMinutes(), 2);
+            return hours + ':' + minutes + ' GMT';
+        };
+    });
+
+/**
+ * I prepend zeros to a number
+ * @param integer n
+ * @param integer len
+ * @return string
+ */
+function prependZeros(n, len) {
+  return (new Array(len + 1).join('0') + n).slice(-len);
+}
--- js/weather-app/controllers/mainController.js
+++ js/weather-app/controllers/mainController.js
@@ -5,13 +5,6 @@ app.controller("mainController", function($scope, $http){
 		$http.jsonp('http://api.openweathermap.org/data/2.5/weather?q=' + $scope.location + '&callback=JSON_CALLBACK')
 			.success(function(data) {
 				$scope.data = data;
-				
-				/*
-				var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
-				d.setUTCSeconds(data.sys.sunrise);				
-				console.log(d);
-				*/
-				
 			});
 	}
 });
