9873c0acf83af985daf2f680022dbaf05a1901d4
Simon Bingham <simon.bingham@ab-uk.com>
2013-08-08 14:05:49 +0100

    no message

--- js/weather-app/controllers/mainController.js
+++ js/weather-app/controllers/mainController.js
@@ -1,15 +1,23 @@
 app.controller("mainController", function($scope, $http){
 	$scope.data = null;
 	$scope.img = null;
-	$scope.location = null;
+	$scope.loc = null;
 	$scope.loader = false;
+	$scope.status = null;
+	
 	$scope.submit = function() {
-		$scope.data = null;
 		$scope.loader = true;
-		$http.jsonp('http://api.openweathermap.org/data/2.5/weather?q=' + $scope.location + '&callback=JSON_CALLBACK')
-			.success(function(data) {
+		$scope.status = null;
+		$http.jsonp('http://api.openweathermap.org/data/2.5/weather?q=' + $scope.loc + '&callback=JSON_CALLBACK')
+			.success(function(data, status) {
 				$scope.data = data;
+				$scope.status = status;
 				$scope.img = 'http://openweathermap.org/img/w/' + data.weather[0]['icon'] + '.png';
+				$scope.loader = false;
+			})
+			.error(function(data, status) {
+				$scope.data = data || 'Oops! The request failed.';
+				$scope.status = status;
 				$scope.loader = false;
 			});
 	}
