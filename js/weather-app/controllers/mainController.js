a968808d30553cac217bdeb819f9f0db23b4a480
Simon Bingham <simon.bingham@ab-uk.com>
2013-08-08 13:32:12 +0100

    no message

--- js/weather-app/controllers/mainController.js
+++ js/weather-app/controllers/mainController.js
@@ -1,12 +1,16 @@
 app.controller("mainController", function($scope, $http){
 	$scope.data = null;
-	$scope.location = null;
 	$scope.img = null;
+	$scope.location = null;
+	$scope.loader = false;
 	$scope.submit = function() {
+		$scope.data = null;
+		$scope.loader = true;
 		$http.jsonp('http://api.openweathermap.org/data/2.5/weather?q=' + $scope.location + '&callback=JSON_CALLBACK')
 			.success(function(data) {
 				$scope.data = data;
 				$scope.img = 'http://openweathermap.org/img/w/' + data.weather[0]['icon'] + '.png';
+				$scope.loader = false;
 			});
 	}
 });
