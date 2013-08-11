3ffc61acf62976829a1f4bfbe04422e35f46ad87
Simon Bingham <me@simonbingham.me.uk>
2013-08-11 08:57:28 +0100

    no message

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 #AngularJS Weather App
 
-A simple weather application built with AngularJS.
+##Overview
 
-Data for the application is sourced from the [OpenWeatherMap](http://openweathermap.org/API) API.
+This is a simple application. Data for the application is sourced from the [OpenWeatherMap](http://openweathermap.org/API) API.
+
+##Technologies
+
+- AngularJS
+- HTML5
+- CSS
+- Bootstrap
+- Modernizr
+
+##Author
+
+This application was developed by [Simon Bingham](https://twitter.com/simonbingham).
--- js/weather-app/controllers/mainController.js
+++ js/weather-app/controllers/mainController.js
@@ -1,24 +1,61 @@
 app.controller("mainController", function($scope, $http){
-	$scope.data = null;
-	$scope.img = null;
-	$scope.loc = null;
+
+	// set default scope variables
+	$scope.data = $scope.img = $scope.loc = $scope.status = null;
+
+	// hide ajax loader image on page load
 	$scope.loader = false;
-	$scope.status = null;
-	
+
+	// process the following code when the form is submitted
 	$scope.submit = function() {
+
+		// display ajax loader image
 		$scope.loader = true;
+
+		// reset status code
 		$scope.status = null;
+
+		// fire ajax request to get weather report for selected location
 		$http.jsonp('http://api.openweathermap.org/data/2.5/weather?q=' + $scope.loc + '&callback=JSON_CALLBACK')
+
+			// the ajax request was successful
 			.success(function(data, status) {
+
+				// store request data in scope
 				$scope.data = data;
+
+				// store status code in scope
 				$scope.status = status;
-				$scope.img = 'http://openweathermap.org/img/w/' + data.weather[0]['icon'] + '.png';
+
+				// the selected location was found
+				if(data.cod == 200) {
+
+					// build image url
+					$scope.img = 'http://openweathermap.org/img/w/' + data.weather[0]['icon'] + '.png';
+
+				// the selected location was not found
+				} else {
+
+					// set error message
+					$scope.data.message = 'Sorry, \'' + $scope.loc + '\' could not be found.';
+					
+				}
+
+				// hide ajax loader image
 				$scope.loader = false;
 			})
+
+			// the ajax request failed
 			.error(function(data, status) {
-				$scope.data = data || 'Oops! The request failed.';
+				// set error message
+				$scope.data = 'Oops! The request failed.';
+
+				// store status code in scope
 				$scope.status = status;
+
+				// hide ajax loader image
 				$scope.loader = false;
 			});
 	}
+
 });
